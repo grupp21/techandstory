b20284c796c48b21d25f512f5ffef89d2efcf291
mai <maje0018@stud.kea.com>
2024-09-10 15:15:38 +0200

    supabase indhold

--- data.js
+++ data.js
@@ -1,8 +1,9 @@
-const url = "https://pbjjrjdyndsqxkaixvrm.supabase.co";
+//const url = "https://pbjjrjdyndsqxkaixvrm.supabase.co/rest/v1/products?select=name,category1";
+const url = "https://pbjjrjdyndsqxkaixvrm.supabase.co/rest/v1/products";
 
 const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBiampyamR5bmRzcXhrYWl4dnJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NzAyNzAsImV4cCI6MjA0MTU0NjI3MH0.rMV_E9Z07FiTskqGWmi3fkRL4vr0nJVIT3QuQQ9Lk70";
 
-fetch(url + "/rest/v1/products", {
+fetch(url, {
   method: "GET",
   headers: {
     apikey: key,

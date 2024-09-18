93e41f2458730248d65108e0e17683fd365a5035
nanajulieaugustine <naje0004@stud.kea.dk>
2024-09-18 14:38:11 +0200

    ændringer i js

--- bookingudstyr.js
+++ bookingudstyr.js
@@ -4,6 +4,6 @@ fetch("https://pbjjrjdyndsqxkaixvrm.supabase.co/rest/v1/products/category")
   .then((response) => response.json())
   .then((categories) => {
     categories.forEach((category) => {
-      categoryList.innerHTML += `<li><a href="produktliste.html?category=${category.category}">${category.category}</a></li>`;
+      categoryList.innerHTML += `<a href="produktliste.html?category=${category.category}">${category.category}</a>`;
     });
   });
--- produktliste.js
+++ produktliste.js
@@ -2,33 +2,31 @@ const params = new URLSearchParams(document.location.search);
 const category = params.get("category");
 let url = undefined;
 
-if (params.has("category1")) {
-  url = `https://pbjjrjdyndsqxkaixvrm.supabase.co/rest/v1/products?category=${category}`;
-} else {
-  url = "https://pbjjrjdyndsqxkaixvrm.supabase.co/rest/v1/products/?limit=50";
-}
+// if (params.has("category1")) {
+//   url = `https://pbjjrjdyndsqxkaixvrm.supabase.co/rest/v1/products?category=${category}`;
+// } else {
+//   url = "https://pbjjrjdyndsqxkaixvrm.supabase.co/rest/v1/products/?limit=50";
+// }
 
-fetch("https://pbjjrjdyndsqxkaixvrm.supabase.co/rest/v1/products")
+/*Fetch data.js?*/
+
+fetch("data.js")
   .then((res) => res.json())
-  .then(showObjects);
+  .then(showProducts);
 
-function showObjects(object) {
-  object.forEach(showObjects);
+function showProducts(product) {
+  object.forEach(showProducts);
 }
 
 function showProduct(object) {
   const template = document.querySelector("#smallProductTemplate").content;
   const copy = template.cloneNode(true);
 
-  copy.querySelector(".name").textContent = object.name;
-  copy.querySelector(".mærke").textContent = object.brand;
-
-  //   const img = copy.querySelector("img");
-  //   img.src = `https://kea-alt-del.dk/t7/images/webp/640/${object.id}.webp`;
-  //   img.alt = object.name;
+  copy.querySelector(".name").textContent = product.name;
+  copy.querySelector(".mærke").textContent = product.brand;
 
   const moreLink = copy.querySelector(".read_more");
-  moreLink.setAttribute("href", `single-view.html?id=${object.id}`);
+  moreLink.setAttribute("href", `single-view.html?id=${product.id}`);
 
   document.querySelector("main").appendChild(copy);
 }
